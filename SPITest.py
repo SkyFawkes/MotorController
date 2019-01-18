acff03aa09b5e0ccc5090e31a114ee619dc16494
Knifyspoony <stevie.wray@hotmail.co.uk>
2019-01-18 23:15:51 +0000

    SPI test extended to include a test of a basic motor controller communications API.

--- SPITest.py
+++ SPITest.py
@@ -7,23 +7,20 @@ spi.open(0, 0) #SPI bus 0, device 0
 spi.max_speed_hz = 1000000 #max speed of 1MHz
 value = 0
 
-# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
-#def readadc(adcnum):
-#        if ((adcnum > 7) or (adcnum < 0)):
-#                return -1
-#        r = spi.xfer2([1,(8+adcnum)<<4,0])
-#        adcout = ((r[1]&3) << 8) + r[2]
-#        return adcout
+def charToInt(x):
+    return ord(x)
 
 while True:
-#    value = readadc(0)
-#    print('Value: '+ str(value))
-#    sleep(1)
-    
-    to_send = [0x01]
-    for to_send[0] in range(0,50) :
-        r = spi.xfer(to_send)
-        print('Sent'+str(to_send))
-        print('Received'+str(r))
+    data_list = [ [],[],[],[] ]
+#    raw = ['S', 'D', 'A', 'V']    
+#    data_out = []
+#    data_out = [charToInt(item) for item in raw]  #convert the characters to numbers
+    data_list[0] = [ord('S'), 50, 100, 150, 200, 0] #test of Speed setting
+    data_list[1] = [ord('D'), 10, 2, 20, 3, 30, 4, 40, 5, 0] #test of distance setting
+    data_list[2] = [ord('A'), 100, 0] #test of acceleration setting
+    data_list[3] = [ord('V'), 9, 0]
+    for data_out in data_list:
+        r = spi.xfer(data_out)
+        print('Sent '+ str(data_out))
+        print('Received '+ str(r))
         sleep(1)
-    to_send = [0x00]
