e0757795206abc92dd7eeeef62ad4b577199e1db
Knifyspoony <stevie.wray@hotmail.co.uk>
2019-02-23 10:39:11 +0000

    Git error - also included the SPITest changes.

--- SPITest.py
+++ SPITest.py
@@ -2,25 +2,51 @@ import spidev
 from time import sleep
 import sys
 
-spi = spidev.SpiDev()
-spi.open(0, 0) #SPI bus 0, device 0
-spi.max_speed_hz = 1000000 #max speed of 1MHz
-value = 0
 
-def charToInt(x):
-    return ord(x)
+class motorController():
 
-while True:
-    data_list = [ [],[],[],[] ]
-#    raw = ['S', 'D', 'A', 'V']    
-#    data_out = []
-#    data_out = [charToInt(item) for item in raw]  #convert the characters to numbers
-    data_list[0] = [ord('S'), 50, 100, 150, 200, 0] #test of Speed setting
-    data_list[1] = [ord('D'), 10, 2, 20, 3, 30, 4, 40, 5, 0] #test of distance setting
-    data_list[2] = [ord('A'), 100, 0] #test of acceleration setting
-    data_list[3] = [ord('V'), 9, 0]
-    for data_out in data_list:
-        r = spi.xfer(data_out)
-        print('Sent '+ str(data_out))
-        print('Received '+ str(r))
-        sleep(1)
+    def __init__(self):
+        self.spi = spidev.SpiDev()
+        self.spi.open(0, 0) #SPI bus 0, device 0
+        self.spi.max_speed_hz = 1000000 #max speed of 1MHz
+
+    def setMotorSpeed(self, speed1,speed2, speed3, speed4):
+        data_out = [ord('S'), speed1,speed2,speed3,speed4, 255] #test of Speed setting
+        recv=self.send(data_out)
+        return recv
+
+	       
+    def setDistance(self, distance1,speed1,distance2, speed2, distance3, speed3, distance4, speed4):
+        data_out = [ord('D'), distance1, speed1, distance2, speed2,distance3, speed3, distance4, speed4, 255]
+        recv=self.send(data_out)
+        return recv
+
+
+    def setMaxAccel(self, maxAccel):
+        data_out = [ord('A'), maxAccel,255]
+        recv=self.send(data_out)
+        return recv
+
+
+    def getADC(self,n):
+        data_out = [ord('V'), n, 255]
+        recv=self.send(data_out)
+        return recv
+	        
+
+    def send(self, data_out):
+        r = self.spi.xfer(data_out)
+#        print('Sent '+ str(data_out))
+        #print('Received '+ str(r))
+        return r
+
+if __name__ =="__main__":
+    myMC=motorController()
+    r=myMC.setMotorSpeed(5,5,5,5)
+#    print('Received', str(r))
+    r=myMC.setDistance(1,2,3,4,5,6,7,8)
+ #   print('Received', str(r))
+    r=myMC.setMaxAccel(1)
+  #  print('Received', str(r))
+    r=myMC.getADC(1)
+    print('Received', str(r))
