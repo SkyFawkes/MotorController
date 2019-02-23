77e604158a259784cfe75dd7610576e1d38db1b5
Knifyspoony <stevie.wray@hotmail.co.uk>
2019-02-23 16:56:05 +0000

    Minor modifications from commissioning communications with the mbed

--- motorController.py
+++ motorController.py
@@ -1,6 +1,7 @@
 import spidev
 from time import sleep
 import sys
+import ctypes
 
 
 class motorController():
@@ -8,16 +9,31 @@ class motorController():
     def __init__(self):
         self.spi = spidev.SpiDev()
         self.spi.open(0, 0) #SPI bus 0, device 0
-        self.spi.max_speed_hz = 1000000 #max speed of 1MHz
+        self.spi.max_speed_hz = 10000 #max speed of 1MHz, practical limit is 10kHz
 
-    def setMotorSpeed(self, speed1,speed2, speed3, speed4):
+    def __del__(self):
+        self.spi.close()
+
+    def setMotorSpeed(self, speed1,speed2, speed3, speed4): #set the motor speed from -100 to + 100
+        speed1=speed1+128
+        speed2=speed2+128
+        speed3=speed3+128
+        speed4=speed4+128
         data_out = [ord('S'), speed1,speed2,speed3,speed4, 255] #test of Speed setting
         recv=self.send(data_out)
         return recv
 
 	       
-    def setDistance(self, distance1,speed1,distance2, speed2, distance3, speed3, distance4, speed4):
-        data_out = [ord('D'), distance1, speed1, distance2, speed2,distance3, speed3, distance4, speed4, 255]
+    def setDistance(self, distance1, distance2, distance3, distance4, speed1, speed2, speed3, speed4): #set the motor distance as a max speed. all in range of -100 to 100
+        speed1=speed1+128
+        speed2=speed2+128
+        speed3=speed3+128
+        speed4=speed4+128
+        distance1=distance1+128
+        distance2=distance2+128
+        distance3=distance3+128
+        distance4=distance4+128
+        data_out = [ord('D'), distance1, distance2, distance3, distance4, speed1, speed2, speed3, speed4, 255]
         recv=self.send(data_out)
         return recv
 
@@ -43,11 +59,12 @@ class motorController():
 
 if __name__ =="__main__":
     myMC=motorController()
-    r=myMC.setMotorSpeed(5,5,5,5)
+    r=myMC.setMotorSpeed(0,0,0,0)
     print('Received', str(r))
-    r=myMC.setDistance(1,2,3,4,5,6,7,8)
+#    r=myMC.setDistance(1,2,3,4,5,6,7,8)
+#    print('Received', str(r))
+#    r=myMC.setMaxAccel(100)
+#    print('Received', str(r))
+    r=myMC.getADC(5)
     print('Received', str(r))
-    r=myMC.setMaxAccel(1)
-    print('Received', str(r))
-    r=myMC.getADC(1)
-    print('Received', str(r))
+#    motorController.spi.close()
